b8a772f830cb85dfa622d2bcd3a97e95d14e9fd0
Matheus Pissaia <pissaia.matheus03@gmail.com>
2022-02-02 00:47:06 -0300

    Criado Abstract Tela

---
+++ limite/abstract_tela.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+
+class AbstractTela(ABC):
+
+    def verifica_opcao(self, mensagem: str = "", inteiros_validos: [] = None):
+        while True:
+            opcao_str = input(mensagem)
+
+            try:
+                opcao_int = int(opcao_str)
+
+                if inteiros_validos and opcao_int not in inteiros_validos:
+                    raise ValueError
+
+                return opcao_int
+
+            except ValueError:
+                print("Valor incorreto: Digite um número inteiro positivo válido")
+                if inteiros_validos:
+                    print("Opções válidas: ", inteiros_validos)
