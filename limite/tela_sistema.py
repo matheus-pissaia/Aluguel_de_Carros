5f614622e2222ce5ca2185902a4c40a562bd0233
Matheus Pissaia <pissaia.matheus03@gmail.com>
2022-02-02 00:46:46 -0300

    Criado Tela Sistema

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,2 +1,17 @@
-class TelaSistema:
-    ...
+from abstract_tela import AbstractTela
+
+
+class TelaSistema(AbstractTela):
+
+    def tela_opcoes(self):
+        print("----- Sistema Aluguel -----")
+        print("Escolha sua opção:")
+        print("1 - Carros")
+        print("2 - Clientes")
+        print("3 - Funcionários")
+        print("4 - Aluguéis")
+        print("0 - Finalizar Sistema")
+
+        opcao = self.verifica_opcao("Digite a opção: ", [1, 2, 3, 4, 0])
+
+        return opcao
