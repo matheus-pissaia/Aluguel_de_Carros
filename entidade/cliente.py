351f90893ac8003eded88cde0af6c0df3e69bc52
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-06 22:53:23 -0300

    Update cliente.py
    Entidade cliente

--- entidade/cliente.py
+++ entidade/cliente.py
@@ -2,21 +2,13 @@ from pessoa import Pessoa
 
 
 class Cliente(Pessoa):
-    def __init__(self, nome: str, cpf: int, telefone: int, idade: int, email: str):
+    def __init__(self, nome: str, cpf: int, telefone: int, idade: int):
         super().__init__(nome, cpf, telefone)
         if isinstance(idade, int):
             self.__idade = idade
         if isinstance(email, str):
             self.__email = email
 
-    @property
-    def email(self) -> str:
-        return self.__email
-
-    @email.setter
-    def email(self, email):
-        if isinstance(email, str):
-            self.__email = email
 
     @property
     def idade(self) -> int:
