2a8906a12c73a24f0ccee79989bc62ac98487133
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-07 17:30:15 -0300

    Update controlador_carros.py
    ajustes

--- controle/controlador_carros.py
+++ controle/controlador_carros.py
@@ -1,17 +1,58 @@
+from limite.tela_Carro import TelaCarro
+from entidade.carro import Carro
 
-class ControlaCarro:
-    
-    def __init__(self):
-        self.__tela_Carro = TelaCarro(self)
+class ControladorCarros():
 
-    def inicia(self):
-        self.tela_inicial()
+  def __init__(self, controlador_sistema):
+    self.__carros = []
+    self.__controlador_sistema = controlador_sistema
+    self.__tela_carro = TelaCarro()
 
-    def cadastrar_carro(self):
-        pass
+  def pega_carro_por_placa(self, placa: str):
+    for carro in self.__carros:
+      if(carro.placa == placa):
+        return livro
+    return None
 
-    def cadastrar_modelo_carro(self):
-        pass
+  def incluir_carro(self):
+    dados_carro = self.__tela_carro.pega_dados_carro()
+    carro = Carro(dados_carro["modelo"], dados_carro["placa"])
+    self.__carros.append(carro)
 
-    def tela_inicial(self):
-        pass
+  def alterar_carro(self):
+    self.lista_carro()
+    placa_carro = self.__tela_carro.seleciona_carro()
+    carro = self.pega_carro_por_placa(placa_carro)
+
+    if(carro is not None):
+      novos_dados_carro = self.__tela_carro.pega_dados_carro()
+      carro.modelo = novos_dados_carro["modelo"]
+      carro.placa = novos_dados_carro["placa"]
+      self.lista_carro()
+    else:
+      self.__tela_carro.mostra_mensagem("carro não existente")
+
+  def lista_carro(self):
+    for carro in self.__carros:
+      self.__tela_carro.mostra_carro({"modelo": carro.modelo, "placa": carro.placa})
+
+  def excluir_carro(self):
+    self.lista_carro()
+    codigo_carro = self.__tela_carro.seleciona_carro()
+    carro = self.pega_carro_por_placa(placa_carro)
+
+    if(carro is not None):
+      self.__carros.remove(carro)
+      self.lista_carro()
+    else:
+      self.__tela_carro.mostra_mensagem(" carro não existente")
+
+  def retornar(self):
+    self.__controlador_sistema.abre_tela()
+
+  def abre_tela(self):
+    lista_opcoes = {1: self.incluir_carro, 2: self.alterar_carro, 3: self.lista_carro, 4: self.excluir_carro, 0: self.retornar}
+
+    continua = True
+    while continua:
+      lista_opcoes[self.__tela_carro.tela_opcoes()]()
