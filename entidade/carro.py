8dbd28674eb157a4b958d2f5e5421e193fd4696f
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-06 22:32:11 -0300

    Update carro.py
    carro

--- entidade/carro.py
+++ entidade/carro.py
@@ -1,2 +1,19 @@
-class Carro:
-    ...
+class carro:
+    
+    def __init__(self, placa_carro: str, modelo_carro: str):
+        self.__placa = placa_carro
+        self.__modelo = modelo_carro
+
+    @property
+    def placa_carro(self):
+        return self.__placa
+    @placa_carro.setter
+    def placa_carro(self, placa):
+        self.__placa = placa
+
+    @property
+    def modelo_carro(self):
+        return self.__modelo
+    @modelo_carro.setter
+    def modelo_carro(self, modelo):
+        self.__modelo = modelo
