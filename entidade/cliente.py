6972faf49268af379998f569e79044a32abc7c2c
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-06 22:16:17 -0300

    Update cliente.py
    cliente

--- entidade/cliente.py
+++ entidade/cliente.py
@@ -1,2 +1,21 @@
-class Cliente:
-    ...
+from Modelo.pessoa import Pessoa
+
+class Cliente(Pessoa):
+    def __init__(self, cpf, nome, idade, email: str, telefone: int):
+        super().__init__(cpf, nome, idade)
+        self.__email = email
+        self.__telefone = telefone
+
+    @property
+    def email(self):
+        return self.__email
+    @email.setter
+    def email(self, email):
+        self.__email = email
+
+    @property
+    def telefone(self):
+        return self.__telefone
+    @telefone.setter
+    def telefone(self, telefone):
+        self.__telefone = telefone
