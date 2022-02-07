1b08ea7fbcec5203ec0cdcd21416bb91955a1bce
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-06 22:06:49 -0300

    Update funcionario.py

--- entidade/funcionario.py
+++ entidade/funcionario.py
@@ -1,2 +1,21 @@
-class Funcionario:
-    ...
+class Funcionario(Pessoa):
+    def __init__(self, cpf, nome, idade, matricula: int, cargo: str):
+        super().__init__(cpf, nome, idade)
+        self.__matricula = matricula
+        self.__cargo = cargo
+
+    @property
+    def matricula(self):
+        return self.__matricula
+    @matricula.setter
+    def matricula(self, matricula):
+        if isinstance(matricula, int):
+            self.__matricula = matricula
+
+    @property
+    def cargo(self):
+        return self.__cargo
+    
+    @cargo.setter
+    def cargo(self, cargo):
+        self.__cargo = cargo
