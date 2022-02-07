760f16f7aed264776777843e974758262a8d9a61
hermelan <41653578+hermelan@users.noreply.github.com>
2022-02-07 00:41:12 -0300

    Update controlador_carros.py
    controlador carros

--- controle/controlador_carros.py
+++ controle/controlador_carros.py
@@ -1,2 +1,17 @@
-class ControladorCarros:
-    ...
+
+class ControlaCarro:
+    
+    def __init__(self):
+        self.__tela_Carro = TelaCarro(self)
+
+    def inicia(self):
+        self.tela_inicial()
+
+    def cadastrar_carro(self):
+        pass
+
+    def cadastrar_modelo_carro(self):
+        pass
+
+    def tela_inicial(self):
+        pass
